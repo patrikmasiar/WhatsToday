25ee2b2b1c13c125e0c7f62a4335a9789797a7f1
massoprod <patrik@kontentino.com>
2019-03-27 16:51:27 +0100

    notes actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -11,4 +11,23 @@ export const SET_WEATHER_CITY = 'SET_WEATHER_CITY';
 export const setWeatherCity = city => ({
   type: SET_WEATHER_CITY,
   payload: city,
+});
+
+export const ADD_TO_NOTES = 'ADD_TO_NOTES';
+export const DELETE_NOTE = 'DELETE_NOTE';
+export const TOOGLE_NOTE = 'TOOGLE_NOTE';
+
+export const addToNote = todos => ({
+  type: ADD_TO_NOTES,
+  payload: todos,
+});
+
+export const deleteNote = payload => ({
+  type: DELETE_NOTE,
+  payload,
+});
+
+export const toogleNote = payload => ({
+  type: TOOGLE_NOTE,
+  payload,
 })
